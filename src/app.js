19b0185dc71be6f97e05419ac2e000085b102b9d
José Ignacio Puig <ipnachoip@gmail.com>
2024-10-04 21:37:54 +0000

    entrega final

--- src/app.js
+++ src/app.js
@@ -6,6 +6,56 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  function generadorCartas() {
+    let palos = ["♦", "♥", "♠", "♣"];
+    let valores = [
+      "A",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K"
+    ];
+
+    function palo(elem) {
+      if (elem === "♥") return "heart";
+      if (elem === "♦") return "diamond";
+      if (elem === "♠") return "spades";
+      if (elem === "♣") return "clubs";
+    }
+
+    let randomElement = array => {
+      let numArray = Math.floor(Math.random() * array.length);
+      return array[numArray];
+    };
+    let randomPalos = randomElement(palos);
+    let randomValores = randomElement(valores);
+
+    let colorPalo = palo(randomPalos);
+
+    document.querySelector(
+      "#card"
+    ).innerHTML = `<div id="top-card" class="${colorPalo}">
+      <p>${randomPalos}</p>
+      </div>
+      <div id="number-card fs-1">
+      <p>${randomValores}</p>
+      </div>
+      <div id="foot-card" class="${colorPalo}">
+      <p class="">${randomPalos}</p>
+      </div>`;
+  }
+
+  document
+    .getElementById("nuevaCarta")
+    .addEventListener("click", generadorCartas);
+  generadorCartas();
+  setInterval(generadorCartas, 8000);
 };
